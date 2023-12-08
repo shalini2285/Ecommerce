564ec475feea9b93d6312bec6a940d0af65ea2b1
shalini2285 <shalinithirugnanasambatham@gmail.com>
2023-12-08 17:04:20 +0530

    Ecommerce

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import './DescriptionBox.css'
+
+const DescriptionBox=()=>{
+    return(
+        <div className="descriptionbox">
+            <div className="descriptionbox-navigator">
+                <div className="description-nav-box">Description</div>
+                <div className="description-nav-box fade">Reviews (122)</div>
+            </div>  
+            <div className="decsriptionbox-description">
+                <p>
+                Ready to take your e-commerce business to the next level? Discover the power of 6valley.
+                 Give your customers the best shopping experience across devices.
+                 Try 6valley today! Top Rated in CodeCanyon. Ready System. Get immediate source code. Easy Installation.
+                </p>
+                <p>Ready to take your e-commerce business to the next level? Discover the power of 6valley. 
+                    Give your customers the best shopping experience across devices.
+                     Try 6valley today! Top Rated in CodeCanyon. Ready System. Get immediate source code. Easy Installation.</p>
+            </div>  
+
+        </div>
+    )
+}
+
+export default DescriptionBox
---
+++ src/Pages/Cart.jsx
@@ -0,0 +1,14 @@
+import React, { useContext } from "react";
+import CartItems from "../Components/CartItems/CartItems";
+import { ShopContext } from "../Context/ShopContext";
+
+const Cart = ()=>{
+    
+    return(
+        <div>
+            <CartItems />
+        </div>
+    )
+}
+
+export default Cart
